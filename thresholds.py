537bd84ee269ea992e337e4ed16c9563d0b34894
verneh <verneh@gmail.com>
2021-07-15 09:51:20 +0200

    Update thresholds.py

--- thresholds.py
+++ thresholds.py
@@ -1,14 +1,14 @@
-# import libaries.
+# Import libaries.
 import cv2 as cv
 
-# thresholds definition.
+# Thresholds definition.
 # https://docs.opencv.org/3.4/db/d8e/tutorial_threshold.html
 
-# read image.
+# Read image.
 img = cv.imread('photos/belle.jpg')
 cv.imshow('Belle', img)
 
-# convert image from one color space to another.
+# cvt = convert image from one color space to another. in this case set it to gray.
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
@@ -16,11 +16,14 @@ cv.imshow('Gray', gray)
 threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY )
 cv.imshow('Simple Thresholded', thresh)
 
+# For the Inverse, we expect that the pixels brighter than the thresh will turn dark
 threshold, thresh_inv = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV )
 cv.imshow('Simple Thresholded Inverse', thresh_inv)
 
-# Adaptive Thresholding
+# Adaptive Thresholding -> method where the threshold value is calculated for smaller regions and therefore, 
+# there will be different threshold values for different regions.
 adaptive_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 11, 9)
 cv.imshow('Adaptive Thresholding', adaptive_thresh)
 
+# Will remove image once key is pressed.
 cv.waitKey(0)
