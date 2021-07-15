100688d4679cf4b06a7e919358558cbd7777cdf8
verneh <verneh@gmail.com>
2021-07-15 10:29:53 +0200

    Add files via upload

---
+++ contours.py
@@ -0,0 +1,34 @@
+# import libaries.
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('/home/verne/Documents/photos/belle.jpg')
+cv.imshow('Belle', img)
+
+# why does the value have to be uint8?
+# https://stackoverflow.com/questions/23749968/why-datatype-has-to-be-uint8-in-opencv-python-wrapper
+blank = np.zeros(img.shape, dtype='uint8')
+cv.imshow('Blank', blank)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
+
+# https://docs.opencv.org/3.4/da/d22/tutorial_py_canny.html
+# multistage edge detection algorithm in order to get the strong edges of the image.
+# dilate an image   
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('Canny Edges', canny)
+
+# ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
+# cv.imshow('Thresh', thresh)
+
+contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+print(f'{len(contours)} contour(s) found!')
+
+cv.drawContours(blank, contours, -1, (0,0,255), 1)
+cv.imshow('Contours Drawn', blank)
+
+cv.waitKey(0)
