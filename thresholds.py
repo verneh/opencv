331c309cbd20efddf449261855e99e1379f2c8b8
verneh <verneh@gmail.com>
2021-07-15 09:36:00 +0200

    Add files via upload

---
+++ thresholds.py
@@ -0,0 +1,23 @@
+# import libaries.
+import cv2 as cv
+
+# read image.
+img = cv.imread('photos/belle.jpg')
+cv.imshow('Belle', img)
+
+# convert image from one color space to another.
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# Simple Thresholding
+threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY )
+cv.imshow('Simple Thresholded', thresh)
+
+threshold, thresh_inv = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV )
+cv.imshow('Simple Thresholded Inverse', thresh_inv)
+
+# Adaptive Thresholding
+adaptive_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 11, 9)
+cv.imshow('Adaptive Thresholding', adaptive_thresh)
+
+cv.waitKey(0)
