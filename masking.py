68c996b37f8d1e5912a092bc3b2d02d886c10363
verneh <verneh@gmail.com>
2021-07-15 12:41:19 +0200

    Update masking.py

--- masking.py
+++ masking.py
@@ -2,7 +2,7 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('/home/verne/Documents/photos/belle.jpg')
+img = cv.imread('photos/belle.jpg')
 cv.imshow('Belle', img)
 
 blank = np.zeros(img.shape[:2], dtype='uint8')
